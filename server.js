110dfa607282d3895dd20f33e18360765c9d26ac
David Simon <me@davidsimon.ch>
2013-11-21 16:29:44 +0100

    Initial Commit & Setup

---
+++ server.js
@@ -0,0 +1,51 @@
+'use strict';
+
+// Module dependencies.
+var express = require('express'),
+    path = require('path'),
+    fs = require('fs');
+
+var app = express();
+
+// Connect to database
+var db = require('./lib/db/mongo');
+
+// Bootstrap models
+var modelsPath = path.join(__dirname, 'lib/models');
+fs.readdirSync(modelsPath).forEach(function (file) {
+  require(modelsPath + '/' + file);
+});
+
+// Populate empty DB with dummy data
+require('./lib/db/dummydata');
+
+// Controllers
+var api = require('./lib/controllers/api');
+
+// Express Configuration
+app.configure(function(){
+	app.use(express.logger('dev'));
+	app.use(express.bodyParser());
+	app.use(express.methodOverride());
+	app.use(app.router);
+});
+
+app.configure('development', function(){
+  app.use(express.static(path.join(__dirname, '.tmp')));
+  app.use(express.static(path.join(__dirname, 'app')));
+  app.use(express.errorHandler());
+});
+
+app.configure('production', function(){
+  app.use(express.favicon(path.join(__dirname, 'public/favicon.ico')));
+  app.use(express.static(path.join(__dirname, 'public', { maxAge: 86400000 /*one day*/ })));
+});
+
+// Routes
+app.get('/api/awesomeThings', api.awesomeThings);
+
+// Start server
+var port = process.env.PORT || 3000;
+app.listen(port, function () {
+  console.log('Express server listening on port %d in %s mode', port, app.get('env'));
+});
