b85ed3a4788a1431f929166595f62e54e3d854e3
David Simon <me@davidsimon.ch>
2013-11-28 17:17:42 +0100

    [Fix] Grunfile - module html

--- Gruntfile.js
+++ Gruntfile.js
@@ -217,7 +217,7 @@ module.exports = function (grunt) {
         files: [{
           expand: true,
           cwd: '<%= yeoman.app %>',
-          src: ['*.html', 'views/*.html'],
+          src: ['*.html', 'views/*.html', 'modules/**/*.html'],
           dest: '<%= yeoman.dist %>'
         }]
       }
