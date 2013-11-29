f318e878a58cb04c4e321bcbaa96fb402deaeeae
David Simon <me@davidsimon.ch>
2013-11-29 15:49:28 +0100

    [Feature] 'Remember Me' functionaility

--- server.js
+++ server.js
@@ -35,13 +35,16 @@ passport.deserializeUser(function(id, done) {
         done(err, user);
     });
 });
-passport.use(new LocalStrategy(function(username, password, done) {
+passport.use(new LocalStrategy({passReqToCallback: true}, function(req, username, password, done) {
     mongoose.model('User').findOne({ username: username }, function(err, user) {
         if (err) { return done(err); }
         if (!user) { return done(null, false, { message: 'Unknown user ' + username }); }
         user.comparePassword(password, function(err, isMatch) {
             if (err) return done(err);
             if(isMatch) {
+                if (req.body.rememberme === true) {
+                    req.session.cookie.expires = false;
+                }
                 return done(null, user);
             } else {
                 return done(null, false, { message: 'Invalid password' });
